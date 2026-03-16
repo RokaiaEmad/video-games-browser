de1fa2bd7a600a254598f9f5db5352717e83c168
rokaia <rokaia.emad@msa.edu.eg>
2026-03-16 23:49:50 +0200

    implement offline cashing using room

--- app/src/main/java/com/example/videogamesbrowser/data/repository/GameRepositoryImpl.kt
+++ app/src/main/java/com/example/videogamesbrowser/data/repository/GameRepositoryImpl.kt
@@ -1,20 +1,26 @@
 package com.example.videogamesbrowser.data.repository
 
+import androidx.paging.ExperimentalPagingApi
 import androidx.paging.Pager
 import androidx.paging.PagingConfig
 import androidx.paging.PagingData
+import androidx.paging.map
 import com.example.videogamesbrowser.data.datasource.MainDataSource
-import com.example.videogamesbrowser.data.paging.GamesPagingSource
-import com.example.videogamesbrowser.data.remote.model.GameDetailsResponse
+import com.example.videogamesbrowser.data.local.GameDao
+import com.example.videogamesbrowser.data.paging.GamesRemoteMediator
 import com.example.videogamesbrowser.data.remote.model.Game
+import com.example.videogamesbrowser.data.remote.model.GameDetailsResponse
 import com.example.videogamesbrowser.domain.repository.GameRepository
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 class GameRepositoryImpl @Inject constructor(
-    private val mainDataSource: MainDataSource
+    private val mainDataSource: MainDataSource,
+    private val gameDao: GameDao
 ) : GameRepository {
 
+    @OptIn(ExperimentalPagingApi::class)
     override fun getGames(): Flow<PagingData<Game>> {
         return Pager(
             config = PagingConfig(
@@ -22,12 +28,19 @@ class GameRepositoryImpl @Inject constructor(
                 prefetchDistance = 2,
                 enablePlaceholders = false
             ),
-            pagingSourceFactory = {
-                GamesPagingSource(mainDataSource)
+            remoteMediator = GamesRemoteMediator(mainDataSource, gameDao),
+            pagingSourceFactory = { gameDao.getGames() }
+        ).flow.map { pagingData ->
+            pagingData.map { entity ->
+                Game(
+                    id = entity.id,
+                    name = entity.name,
+                    backgroundImage = entity.backgroundImage,
+                    rating = entity.rating
+                )
             }
-        ).flow
+        }
     }
-
 
     override suspend fun getGameDetails(id: Int): GameDetailsResponse {
         return mainDataSource.getGameDetails(id)
