32f1e3c700cd6ea62fb48bd80cb85abf29837135
rokaia <rokaia.emad@msa.edu.eg>
2026-03-16 04:00:57 +0200

    refactor: move default genre and page size to Constants

--- app/src/main/java/com/example/videogamesbrowser/domain/usecase/GetGameUseCase.kt
+++ app/src/main/java/com/example/videogamesbrowser/domain/usecase/GetGameUseCase.kt
@@ -3,6 +3,7 @@ package com.example.videogamesbrowser.domain.usecase
 
 import com.example.videogamesbrowser.data.remote.model.Game
 import com.example.videogamesbrowser.domain.repository.GameRepository
+import com.example.videogamesbrowser.utils.Constants
 import javax.inject.Inject
 
 class GetGameUseCase @Inject constructor(
@@ -10,8 +11,8 @@ class GetGameUseCase @Inject constructor(
 ) {
     suspend operator fun invoke(
         page: Int = 1,
-        genre: String = "action",
-        pageSize: Int = 6
+        genre: String = Constants.DEFAULT_GENRE,
+        pageSize: Int = Constants.PAGE_SIZE
     ): List<Game> {
         return gameRepository.getGames(
             genre = genre,
--- app/src/main/java/com/example/videogamesbrowser/utils/Constants.kt
+++ app/src/main/java/com/example/videogamesbrowser/utils/Constants.kt
@@ -1,11 +1,8 @@
 package com.example.videogamesbrowser.utils
 
 object Constants {
-
     const val BASE_URL = "https://api.rawg.io/api/"
     const val API_KEY = "eeb3dcfcf990470d85ed13f7a99f39ec"
-
-    const val DEFAULT_PAGE_SIZE = 20
     const val DEFAULT_GENRE = "action"
-
+    const val PAGE_SIZE = 6
 }
